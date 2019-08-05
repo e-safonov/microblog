6c28cc7e5672375cf0cc6293207245f0c02cccf5
Evgeny Safonov <evgeny.safonov@gost-group.com>
2019-08-05 17:06:32 +0300

    Charper 7 of Mega-Tutorial.

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {'connect_args': {'check_same_thread': False}}
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
--- venv_patch.sh
+++ venv_patch.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo "export FLASK_APP=microblog.py" >> ./venv/bin/activate
+echo "export FLASK_DEBUG=1" >> ./venv/bin/activate
